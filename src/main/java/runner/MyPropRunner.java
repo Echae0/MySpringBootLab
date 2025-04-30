eeded47de255c723cae8e1dc2363a3144016e93c
Echae0 <edujcm01@gmail.com>
2025-05-01 00:18:25 +0900

    MyPropRunner update

--- src/main/java/runner/MyPropRunner.java
+++ src/main/java/runner/MyPropRunner.java
@@ -1,9 +1,15 @@
 package runner;
 
+import config.MyEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import property.MyPropProperties;
 
 @Component
 public class MyPropRunner implements ApplicationRunner {
@@ -13,10 +19,29 @@ public class MyPropRunner implements ApplicationRunner {
     @Value("${myprop.port}")
     private int port;
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyEnvironment myEnvironment;
+
+    @Autowired
+    private MyPropProperties properties;
+
+    private Logger logger = LoggerFactory.getLogger(MyPropRunner.class);
+
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        System.out.println("myprop.name = " + name);
-        System.out.println("myprop.port = " + port);
+        logger.debug("myprop.name = {}", name);
+        logger.debug("myprop.port = {}", port);
+        logger.debug("${myprop.fullName} = {}", environment.getProperty("myprop.fullName"));
+
+
+        logger.info("MyPropProperties getName() = {}", properties.getName());
+        logger.info("MyPropProperties getPort() = {}",  properties.getPort());
+        logger.info("설정된 Port 번호 = {}", environment.getProperty("local.server.port"));
+
+        logger.info("현재 활성화된 Bean = {}", myEnvironment);
 
         args.getOptionNames()  //Set<String>
                 .forEach(name -> System.out.println(name));

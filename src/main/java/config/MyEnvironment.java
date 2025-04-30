8cdeccca624e9a5c95077c971012f698c1bda68d
Echae0 <edujcm01@gmail.com>
2025-05-01 00:12:08 +0900

    application properties add

--- src/main/java/config/MyEnvironment.java
+++ src/main/java/config/MyEnvironment.java
@@ -1,5 +1,12 @@
 package config;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
 public class MyEnvironment {
     String mode;
 }
--- src/main/java/config/TestConfig.java
+++ src/main/java/config/TestConfig.java
@@ -1,4 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
 public class TestConfig {
+    @Bean
+    public MyEnvironment myEnvironment(){
+        return MyEnvironment.builder()
+                .mode("테스트환경")
+                .build();
+    }
 }

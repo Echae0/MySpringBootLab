a86232f8ee1e47e6bdcb6c89924aaa77491e83e2
Echae0 <edujcm01@gmail.com>
2025-05-01 00:25:19 +0900

    run with jar

--- src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
+++ src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
@@ -1,13 +1,22 @@
 package com.rookies3.myspringbootlab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringbootLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringbootLabApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringbootLabApplication.class);
+
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
+	@Bean
+	public String hello() {
+		return "hello springboot";
+	}
 }

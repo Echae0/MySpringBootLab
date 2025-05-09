2840934ff2cdc33370c3749bac5ae1759b5e03db
Echae0 <edujcm01@gmail.com>
2025-05-09 22:43:47 +0900

    add exception

--- src/main/java/com/rookies3/myspringbootlab/entity/Book.java
+++ src/main/java/com/rookies3/myspringbootlab/entity/Book.java
@@ -1,33 +1,33 @@
 package com.rookies3.myspringbootlab.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
+@Table(name = "books")
+@Getter @Setter
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(unique = true, nullable = false)
     private String isbn;
+
+    @Column(nullable = false)
     private Integer price;
+
+    @Column(nullable = false)
     private LocalDate publishDate;
 
-    public Book(String title, String author, String isbn, Integer price, LocalDate publishDate) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.price = price;
-        this.publishDate = publishDate;
-    }
 }

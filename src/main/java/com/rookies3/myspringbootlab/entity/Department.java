9291f8a97a159d0314c505e7f95d85793307fc19
Echae0 <edujcm01@gmail.com>
2025-05-09 23:29:01 +0900

    entity update

--- src/main/java/com/rookies3/myspringbootlab/entity/Department.java
+++ src/main/java/com/rookies3/myspringbootlab/entity/Department.java
@@ -1,15 +1,28 @@
 package com.rookies3.myspringbootlab.entity;
 
-import java.util.ArrayList;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable = false)
     private String name;
-    
+
+    @Column(unique = true, nullable = false)
     private String code;
-    
-    private List<Student> students = new ArrayList<>();
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students;
 }
--- src/main/java/com/rookies3/myspringbootlab/entity/Student.java
+++ src/main/java/com/rookies3/myspringbootlab/entity/Student.java
@@ -1,11 +1,26 @@
 package com.rookies3.myspringbootlab.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
-    
+
+    @Column(unique = true, nullable = false)
     private String studentNumber;
-    
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
     private Department department;
 }
